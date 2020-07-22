aec7b0541556ce56a106ec28395a68444808533e
neelamparekh <neelamsoni99@yahoo.co.in>
2020-07-22 09:14:42 -0400

    Added JS and index Files

---
+++ JS/main.js
@@ -0,0 +1,104 @@
+// var - Is a keyword - used for? - It is used for init variables - holds a value
+
+// let and const = var
+
+// var a = 10; // number; // ES5 - Not suggested to use anymore
+// var b = 'Nilam' // String
+// var c = true; // Boolean
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+
+// var d = {}; // Object
+// var e = []; // Array
+
+
+
+
+// var a; // DEFAULT VALUE = Undefined
+// console.log(a);
+
+// var b = null;
+// b = 10;
+// b = 'Nilam';
+// console.log(b); // Nilam - gets printed
+
+// var b = 123.456; // number // typeof
+
+// var s = "Nilam";
+// var t = "D'cruz"; // " " is a very good practice
+
+// variable can not start with special characters - it can be _kjrf87
+
+// var a = 10;
+// var b = 20;
+// var result = a+b; // a-b, a*b, a/b, a%b
+
+// console.log(a+b);
+// console.log(result);
+
+// ++ Inc --> 2 types --> Post Inc, Pre Inc
+// -- Dec --> 2 types --> Post Dec, Pre Dec
+
+
+var a = 10;
+// a++; // Post Inc
+// ++a; // Pre Inc
+//console.log(a);
+
+//console.log(a++); //10
+//console.log(a); //11
+
+//console.log(++a); //11
+//console.log(a); //11
+
+// console.log(a--); // 10
+// console.log(a); // 9
+
+// console.log(--a); //9
+// console.log(a); //9
+
+
+//CONDITION
+//if (false) { //Sun rises in West?
+  //  console.log("YES");// YES
+//} else {
+  // console.log("NO"); // NO
+//}
+
+var a = 10; // assignment operator
+var b = 20;  // '10'
+
+// >
+// <
+// ==
+// ===
+// >=
+//<=
+
+// !== --> It will compare only values
+// !=== --> It will check for the type
+
+// == -> It will compare only values
+//=== -> It will check for the type strictly
+
+if(a>b) { // false // (a<b), (a==b), (a===b), (a!=b), (a>=b)
+    console.log("YES"); //YES
+} else {
+    console.log("NO"); // NO
+}
+
+
+
+
+
+
+
+
+
+
